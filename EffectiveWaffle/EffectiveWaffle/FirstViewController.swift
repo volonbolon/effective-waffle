67f055a5103ab8a4b96437c2f13601a5bca4e14c
Ariel Rodriguez <ariel@volonbolon.net>
2015-12-14 16:36:13 -0300

    Add Xcode Project

---
+++ EffectiveWaffle/EffectiveWaffle/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EffectiveWaffle
+//
+//  Created by Ariel Rodriguez on 12/14/15.
+//  Copyright © 2015 Ariel Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
