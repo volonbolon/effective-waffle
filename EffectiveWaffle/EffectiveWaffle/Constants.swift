128c9be137cfc440696ed769f1392a3be6e4f202
Ariel Rodriguez <ariel@volonbolon.net>
2015-12-14 20:35:30 -0300

    Add Constants

---
+++ EffectiveWaffle/EffectiveWaffle/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  EffectiveWaffle
+//
+//  Created by Ariel Rodriguez on 12/14/15.
+//  Copyright © 2015 Ariel Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct APIKey {
+        static let YouTube = "AIzaSyAjc6jfDSOg1w4dwz2n2ZKyh1k4wxT1DvM"
+    }
+    struct SegueIdentifier {
+        static let ShowPlaylist = "ShowPlaylist"
+    }
+}
