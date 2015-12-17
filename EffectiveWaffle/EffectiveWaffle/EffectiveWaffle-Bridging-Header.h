67f0cbc5d8096b8329cf3c732c8fd9ca0b9a827b
Ariel Rodriguez <ariel@volonbolon.net>
2015-12-17 19:56:40 -0300

    Failing with CocoaPods

---
+++ EffectiveWaffle/EffectiveWaffle/EffectiveWaffle-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  EffectiveWaffle-Bridging-Header.h
+//  EffectiveWaffle
+//
+//  Created by Ariel Rodriguez on 12/17/15.
+//  Copyright © 2015 Ariel Rodriguez. All rights reserved.
+//
+
+#ifndef EffectiveWaffle_Bridging_Header_h
+#define EffectiveWaffle_Bridging_Header_h
+
+#import "YTPlayerView.h"
+
+#endif /* EffectiveWaffle_Bridging_Header_h */
